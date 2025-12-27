833be3bc87f5c79bbb8f8931af83be43a26ac12c
yassinoli <yassin890890.890@gmail.com>
2025-12-27 19:52:25 +0100

    setup levels

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.querySelector('.starButton').onclick = startGame
 scor()
 bullets()
 level()
-let bulcount = 6
+let bulcount = 100
 let hero
 let enemies = []
 let enemytotal = 0
@@ -28,6 +28,9 @@ let enemycount = 0
 let lvl = 1
 let scorr = 0
  let speed = 1.3
+ const enemy1 = 'enemy1.png'
+ const enemy2 = 'enemy2.png'
+ const enemy3 = 'enemy3.png'
 
 //--------------
 let bulcounter = document.querySelector('.countBoullet')
@@ -44,7 +47,9 @@ function startGame() {
   startMenu.remove()
   createHero()
   createnemy()
-  setInterval(createnemy, 4000)
+  bonus()
+  setInterval(createnemy, 400)
+
 }
 
 function createHero() {
@@ -65,12 +70,19 @@ document.addEventListener('keydown', e => {
 })
 
 function createnemy() {
+  let enemyto
+  if (enemytotal<=6) enemyto = enemy1
+  if (enemytotal>6 && enemytotal<=20) enemyto = enemy2
+  if (enemytotal >20) {
+    lastStage()
+    return
+   }
   if (gameOver) return
-   if (enemytotal>4*lvl) lvl++ , bulcount = parseInt((20*lvl)/1.5) , 
-   levels.innerText = `LEVEL ${lvl}`
-  if (enemycount == 3*lvl || enemytotal==5*lvl) return 
+   if (enemytotal>6 && lvl <2) lvl++ , levels.innerText = `LEVEL ${lvl}` , speed = 3 // lvl & -> 2
+   if (enemytotal>20 && lvl <3) lvl++ , levels.innerText = `LEVEL ${lvl}` // lvl 2 -> 3
+  if (enemycount == 3*lvl ) return 
   const e = document.createElement('img')
-  e.src = 'enemy1.png'
+  e.src = enemyto
   e.className = 'enmySize'
   e.style.left = Math.random() * (game.clientWidth - 80) + 'px'
   enemycount++
@@ -103,9 +115,10 @@ let dir = 1
 
 // -hero bul
 function heroshut() {
-   bulcount--
    
-  if (bulcount<0) return;
+   
+  if (bulcount<=0) return;
+  bulcount--
   bulcounter.innerText =  `Bullets ${bulcount}`
   levels.innerText = `LEVEL ${lvl}`
   const b = document.createElement('div')
@@ -185,6 +198,10 @@ function enemybuletmove(b) {
       return
     }
 
+    if (!hit(b,game)){
+      b.remove()
+    }
+
     // enemybulet <-> hero bulet
     heroBullets.forEach((hb, i) => {
       if (hit(b, hb)) {
@@ -244,12 +261,123 @@ function level() {
 }
 
 function bonus(){
-  let bons = document.createElement('div')
+ setInterval(() => {
+   let bons = document.createElement('div')
   bons.className = 'bonus'
   bons.innerText = '+5'
-  document.querySelector('.startdiv').appendChild(bons)
+  bulcounter.innerText =  `Bullets ${bulcount}`
+  game.appendChild(bons)
+  bonsmove()
+}, 10000);
 }
 
-// setInterval(() => {
-//   bonus()
-// }, 3000);
+function bonsmove(){
+  let wg = game.getBoundingClientRect()
+  let bons = document.querySelector('.bonus')
+   bons.style.left = Math.random()*wg.width + 'px'
+  bons.style.top = 5 + 'px'
+  
+
+ function mvbns(){ 
+  if (hit(bons,hero)){
+    bons.remove()
+    bulcount += 5
+     bulcounter.innerText =  `Bullets ${bulcount}`
+  }
+
+  if (!hit(bons , game)){
+    bons.remove()
+  }
+
+   bons.style.top = bons.offsetTop + speed + 'px'
+   requestAnimationFrame(mvbns)
+  }
+     requestAnimationFrame(mvbns)
+}
+
+
+
+
+function lastStage(){
+ if(enemies.length == 0) {
+    const e = document.createElement('img')
+  e.src = enemy3
+  e.className = 'enmySize'
+  e.style.left = Math.random() * (game.clientWidth - 80) + 'px'
+  enemycount++
+  enemytotal++
+  game.appendChild(e)
+  enemies.push(e)
+
+  movitt(e)
+  shutitt(e)}
+}
+
+function movitt(enemy) {
+  
+let dir = 1
+  function step() {
+    if (!enemy.parentElement || gameOver) return
+
+    let x = enemy.offsetLeft + dir * 4
+    enemy.style.left = x + 'px'
+
+    if (x <= 0 || x >= game.clientWidth - enemy.offsetWidth) {
+      dir *= -1
+    }
+    requestAnimationFrame(step)
+  }
+  requestAnimationFrame(step)
+}
+
+function shutitt(enemy) {
+  const shoot = setInterval(() => {
+    if (!enemy.parentElement || gameOver) {
+      clearInterval(shoot)
+      return
+    }
+
+    const b = document.createElement('div')
+    b.className = 'bullet enemyBullet'
+
+    b.style.left = enemy.offsetLeft + enemy.offsetWidth / 2 - 4 + 'px'
+    b.style.top = enemy.offsetTop + enemy.offsetHeight + 'px'
+
+    game.appendChild(b)
+    enemyBullets.push(b)
+    lastbullet(b)
+  }, 2000)
+}
+
+function lastbullet(b) {
+  function step() {
+    if (!b.parentElement || gameOver) return
+
+    b.style.top = b.offsetTop + 16 + 'px'
+
+    // enemybullet -> hero
+    if (hit(b, hero)) {
+      heroDies()
+      b.remove()
+      return
+    }
+
+    // enemybulet <-> hero bulet
+    heroBullets.forEach((hb, i) => {
+      if (hit(b, hb)) {
+        b.remove()
+        hb.remove()
+        heroBullets.splice(i, 1)
+      }
+    })
+
+    requestAnimationFrame(step)
+  }
+  requestAnimationFrame(step)
+}
+
+
+
+setInterval(() => {
+  console.log(enemyBullets.length);
+}, 3000);
